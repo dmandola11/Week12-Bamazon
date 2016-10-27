11b7c615485d20971cf59ab2274e90fff9fdee93
Damian Mandola Jr <damianjr@Damian-Mandola-Jrs-MacBook-Pro.local>
2016-10-27 16:51:39 -0500

    added mysql connection and started inquirer prompt function to begin order query

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,1 +1,32 @@
+
+var mysql = require('mysql');
 var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "",
+	database: "Bamazon"
+});
+
+connection.connect(function(error){
+	if (error) throw error;
+	console.log("connected as id " + connection.threadId);
+});
+
+var startOrder = function() {
+    connection.query('SELECT * FROM Products', function(error, result) {
+        console.log(result);
+        inquirer.prompt({
+            name: "choice",
+            type: "rawlist",
+            choices: function(value) {
+                var productArray = [];
+                for (var i = 0; i < result.length; i++) {
+                    productArray.push(result[i].itemname);
+                }
+                return productArray;
+            },
+            message: "What auction would you like to place a bid in?"
+        }).then(function(answer) {
